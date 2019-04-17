480a86c1045da9678cf41684fabc6dba0736031d
unknown <you@example.com>
2019-04-17 04:32:42 +0300

    improved

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>it4 OSC</title>
+</head>
+<body>
+<h1>welcome itiwoch</h1>
+</body>
+</html>>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/profile', function ()
+
+{
+return view('profile')
+
+}
+);
